5bdd41891972035b4a188fd138b3a0de3d7d5402
VitaliiZvieriev <zverew91@gmail.com>
2025-04-12 00:32:33 +0300

    add secrets section

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 const mobMenuBtns = document.querySelectorAll('.js-mob-menu-btn');
 const mobMenu = document.querySelector('.js-header-mob-menu');
 const mobLink = document.querySelectorAll('.js-mob-nav-link');
+const header = document.querySelector('.js-header');
 
-const toggleMobMenu = () => mobMenu.classList.toggle('is-open');
+const toggleMobMenu = () => {
+  mobMenu.classList.toggle('is-open');
+  mobMenu.classList.contains('is-open')
+    ? (header.style.position = 'static')
+    : (header.style.position = 'sticky');
+};
 
 mobMenuBtns.forEach(btn => btn.addEventListener('click', toggleMobMenu));
 mobLink.forEach(btn => btn.addEventListener('click', toggleMobMenu));
