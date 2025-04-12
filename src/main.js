46fe8baf9386fa09fae31b038af344e3931e3b12
VitaliiZvieriev <zverew91@gmail.com>
2025-04-12 04:32:12 +0300

    add cookie modal

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const mobMenuBtns = document.querySelectorAll('.js-mob-menu-btn');
 const mobMenu = document.querySelector('.js-header-mob-menu');
 const mobLink = document.querySelectorAll('.js-mob-nav-link');
 const header = document.querySelector('.js-header');
+const cookieModal = document.querySelector('.js-cookies');
+const cookiesBtn = cookieModal.querySelectorAll('.cookie-btn');
 
 const toggleMobMenu = () => {
   mobMenu.classList.toggle('is-open');
@@ -10,5 +12,14 @@ const toggleMobMenu = () => {
     : (header.style.position = 'sticky');
 };
 
+const closeCookieModal = () => {
+  cookieModal.classList.remove('is-show');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => cookieModal.classList.add('is-show'), 700);
+});
+
 mobMenuBtns.forEach(btn => btn.addEventListener('click', toggleMobMenu));
 mobLink.forEach(btn => btn.addEventListener('click', toggleMobMenu));
+cookiesBtn.forEach(btn => btn.addEventListener('click', closeCookieModal));
