e4f0fcd3c50143ea7e421e4679786b6945477f6a
VitaliiZvieriev <zverew91@gmail.com>
2025-04-15 17:53:47 +0300

    fix #1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 const mobMenuBtns = document.querySelectorAll('.js-mob-menu-btn');
 const mobMenu = document.querySelector('.js-header-mob-menu');
-const mobLink = document.querySelectorAll('.js-mob-nav-link');
+const navLink = document.querySelectorAll('.js-nav-link');
 const header = document.querySelector('.js-header');
 const cookieModal = document.querySelector('.js-cookies');
-const cookiesBtn = cookieModal.querySelectorAll('.cookie-btn');
+const cookiesBtn = cookieModal.querySelectorAll('.js-cookie-btn');
 
 const toggleMobMenu = () => {
   mobMenu.classList.toggle('is-open');
@@ -12,15 +12,38 @@ const toggleMobMenu = () => {
     : (header.style.position = 'sticky');
 };
 
+const handleScroll = e => {
+  e.preventDefault();
+
+  const targetEl = document.querySelector(e.currentTarget.getAttribute('href'));
+
+  mobMenu.classList.remove('is-open');
+  header.style.position = 'sticky';
+
+  if (!targetEl) return;
+
+  const targetPosition =
+    targetEl.getBoundingClientRect().top + window.scrollY - header.offsetHeight;
+
+  window.scrollTo({
+    top: targetPosition,
+    behavior: 'smooth',
+  });
+};
+
 const closeCookieModal = () => {
   cookieModal.classList.remove('is-show');
+  localStorage.setItem('firstLoad', 'fasle');
   cookiesBtn.forEach(btn => btn.removeEventListener('click', closeCookieModal));
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => cookieModal.classList.add('is-show'), 700);
+  localStorage.getItem('firstLoad') ??
+    setTimeout(() => {
+      cookieModal.classList.add('is-show');
+    }, 700);
 });
 
 mobMenuBtns.forEach(btn => btn.addEventListener('click', toggleMobMenu));
-mobLink.forEach(btn => btn.addEventListener('click', toggleMobMenu));
+navLink.forEach(btn => btn.addEventListener('click', handleScroll));
 cookiesBtn.forEach(btn => btn.addEventListener('click', closeCookieModal));
